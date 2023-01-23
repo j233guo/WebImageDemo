67b2cb80bf7dd367e7975429c07fc358444edc59
Jiaming Guo <j23guo@gmail.com>
2023-01-23 12:46:42 -0500

    Initial Commit

---
+++ WebImageDemo/WebImageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebImageDemoApp.swift
+//  WebImageDemo
+//
+//  Created by Jiaming Guo on 2023-01-23.
+//
+
+import SwiftUI
+
+@main
+struct WebImageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
